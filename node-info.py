b1146f06ecdbc71015e6c660a4b125809e96dac0
dcrypt11 <dcrypt111@gmail.com>
2023-11-26 16:58:44 +0200

    Improve balance fetching; Add disk usage

--- node-info.py
+++ node-info.py
@@ -4,8 +4,12 @@ import screen_ocr
 import re
 import json
 
+current_balance = 0
 
-def get_data():
+
+def get_data(old_balance):
+    global current_balance
+
     ocr_reader = screen_ocr.Reader.create_quality_reader()
     # To read a cropped region, add bounding_box=(left, top, right, bottom).
     result = ocr_reader.read_screen(bounding_box=(1300, 0, 1900, 400))
@@ -16,8 +20,10 @@ def get_data():
     balance = None
     cpu = None
     ram = None
+    disk = None
 
     balance_index = None
+    balance_string = None
 
     line_index = 0
     for line in lines:
@@ -28,9 +34,9 @@ def get_data():
                 transactions = None
         if 'Balance' in line:
             try:
-                balance = float(line.replace('ETCPOW Balance: ', '').replace(',', '.').strip().replace(' ', '.'))
+                balance_string = line.replace('ETCPOW Balance: ', '')
             except:
-                balance = None
+                balance_string = None
                 balance_index = line_index
         if 'CPU' in line:
             try:
@@ -42,22 +48,38 @@ def get_data():
                 ram = float(line.replace('RAM: ', '').replace('%', '').replace('/0', '').replace(' ', '.'))
             except:
                 ram = None
+        if 'DISK' in line:
+            try:
+                disk = float(line.replace('DISK: ', '').replace('%', '').replace('/0', '').replace(' ', '.'))
+            except:
+                disk = None
+
         line_index += 1
 
-    if balance is None and balance_index is not None:
+    if balance_string is None and balance_index is not None:
         try:
-            balance = float(lines[balance_index + 1])
+            balance_string = lines[balance_index + 1]
         except:
-            balance = None
+            balance_string = None
+
+    try:
+        balance = float(balance_string.replace(',', '.').strip().replace(' ', '.'))
+    except:
+        balance = None
+
+    if old_balance > 1 and balance > (old_balance * 50):
+        balance = None
 
     data = {
         "transactions": transactions,
         "balance": balance,
         "cpu": cpu,
-        "ram": ram
+        "ram": ram,
+        "disk": disk
     }
 
-    print(data)
+    if balance is not None:
+        current_balance = balance
 
     return data
 
@@ -69,8 +91,9 @@ urls = (
 
 class index:
     def GET(self):
+        global current_balance
         web.header('Content-Type', 'application/json')
-        return json.dumps(get_data())
+        return json.dumps(get_data(current_balance))
 
 
 if __name__ == "__main__":
